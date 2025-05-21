1b4fbb6a96ad1d0ab84f0df95d3d73b0ddefafbf
Dave <publiclyviewed@gmail.com>
2025-05-21 14:52:45 +0700

    Initial update

--- server/server.js
+++ server/server.js
@@ -1,11 +1,26 @@
 // server/server.js
 const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv'); // Import dotenv
+const connectDB = require('./config/db'); // Import the database connection function
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
 const app = express();
-const PORT = process.env.PORT || 5000; 
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello from the backend!');
 });
+
+// We'll add more routes here later
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
